633b78a677cdac5536c62b48480cbe834a67b2d0
majornuts <mads.fisker@gmail.com>
2025-03-18 07:33:22 +0100

    better query for db.

--- lib/data/DataApi.dart
+++ lib/data/DataApi.dart
@@ -5,7 +5,8 @@ class DataApi {
   static Future<FT> fetchDataTreesParser() async {
     var dio = Dio();
     const urlTrees =
-        'https://wfs-kbhkort.kk.dk/ows?service=wfs&version=2.0.0&request=GetFeature&typeName=k101:gadetraer&outputFormat=json&SRSNAME=EPSG:4326';
+        // 'https://wfs-kbhkort.kk.dk/ows?service=wfs&version=2.0.0&request=GetFeature&typeName=k101:gadetraer&outputFormat=json&SRSNAME=EPSG:4326';
+        'https://wfs-kbhkort.kk.dk/ows?service=wfs&version=2.0.0&request=GetFeature&typeName=k101:gadetraer&outputFormat=json&SRSNAME=EPSG:4326&propertyName=slaegt,slaegtsnavn,planteaar,wkb_geometry,dansk_navn,id';
     final response = await dio.get(urlTrees);
     return FT.fromJson(response.data);
   }
